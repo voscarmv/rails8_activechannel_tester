fc863dffba398a284cc18746b5f989bbccc3b6a6
Oscar Mier <voscarmv@gmail.com>
2025-04-29 20:01:15 -0500

    Succesful upload notice received from solid channelgit add .

--- consumemsg.js
+++ consumemsg.js
@@ -1,4 +1,5 @@
 import WebSocket from 'ws';
+// import { createConsumer } from "@rails/actioncable";
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -28,16 +29,46 @@ dotenv.config();
         const cableUrl = `${process.env.WS}/cable?token=${token}`;
 
         const ws = new WebSocket(cableUrl);
-        
+
         ws.on('open', function open() {
-          console.log('Connected to the server');
-          // Send a message to the server
-          ws.send(JSON.stringify({ type: 'greeting', payload: 'Hello from Node.js client!' }));
+            console.log('Connected to the server');
+            // Send a message to the server
+            // ws.send(JSON.stringify({ type: 'greeting', payload: 'Hello from Node.js client!' }));
+            const msg = {
+                command: "subscribe",
+                identifier: JSON.stringify({
+                  channel: "NotificationsChannel",
+                //   room: "general"
+                })
+              };
+              ws.send(JSON.stringify(msg));
         });
-        
+
         ws.on('message', function incoming(data) {
           console.log('Received:', data.toString());
         });
+
+        // const consumer = createConsumer(cableUrl);
+        // const subscription = consumer.subscriptions.create(
+        //     { channel: "Notifications" },
+        //     {
+        //         connected() {
+        //             console.log("Connected to ChatChannel.");
+        //         },
+        //         disconnected() {
+        //             console.log("Disconnected from ChatChannel.");
+        //         },
+        //         received(data) {
+        //             console.log("Received:", data);
+        //         },
+        //         // speak(message) {
+        //         //     this.perform("speak", { message: message });
+        //         // },
+        //     }
+        // );
+
+        //         // To send a message:
+        // subscription.speak("Hello, world!");
     }
 )();
 
